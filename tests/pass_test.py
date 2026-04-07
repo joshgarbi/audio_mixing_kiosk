383af8a89fcd816978a74a273ec157f2b6971a3f
Joshua Garbi <joshua.garbi@gmail.com>
2026-04-07 09:48:30 -0700

    fixing ci tests

--- tests/pass_test.py
+++ tests/pass_test.py
@@ -1,5 +1,13 @@
 from password_manager import new_pass, verify_pass
 
+import os
+import keyring
+
+# Check if running in a CI environment
+if os.getenv("CI"):
+    from keyrings.alt.file import PlaintextKeyring
+    keyring.set_keyring(PlaintextKeyring())
+
 def test_password_hashing_and_verification():
     password = "my_secure_password"
     new_pass("test_user", password)
