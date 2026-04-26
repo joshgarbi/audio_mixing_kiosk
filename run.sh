18839f24aadfdd19b4362a73f3f1292432e6ebab
Josh Garbi <joshua.garbi@gmail.com>
2026-04-26 08:56:30 -0700

    hiding coursor

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 echo "Launching GUI..."
 # 'sg' allows us to run the command with the new group permissions immediately
 export WLR_RENDERER=pixman
-export XCURSOR_SIZE=0
 sg video -c "sg render -c 'cage -s -- .venv/bin/python3 src/gui.py'"
